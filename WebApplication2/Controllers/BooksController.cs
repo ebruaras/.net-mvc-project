4fe5ecafcc778b638a78e3ec443853d6bd9d0bbb
ebruaras <ebruaras.96@hotmail.com>
2020-04-18 18:21:52 +0300

    Initial commit

---
+++ WebApplication2/Controllers/BooksController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class BooksController : Controller
+    {
+        List<WebApplication2.Models.Book> bookList = new List<WebApplication2.Models.Book>()
+{
+        new WebApplication2.Models.Book()
+    {
+        baslik = "MVC Dünyasına Merhaba",
+        aciklama = "MVC adına herşey",
+        fiyat = 100,
+        stokadedi = 100,
+        tur = "Web programlama",
+        yayintarihi = DateTime.Now
+    },
+         new WebApplication2.Models.Book()
+    {
+        baslik = "MVC Dünyasına Merhaba",
+        aciklama = "MVC adına herşey",
+        fiyat = 100,
+        stokadedi = 100,
+        tur = "Web programlama",
+        yayintarihi = DateTime.Now
+    },
+          new WebApplication2.Models.Book()
+    {
+        baslik = "MVC Dünyasına Merhaba",
+        aciklama = "MVC adına herşey",
+        fiyat = 100,
+        stokadedi = 100,
+        tur = "Web programlama",
+        yayintarihi = DateTime.Now
+    }
+    };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult create()
+        {
+            List<string> tur = new List<string>()
+            {
+                "Macera","Polisiye","Edebiyat","Tarih"
+            };
+            SelectList turler = new SelectList(tur);
+            ViewData["tur"] = turler;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult create(Book book)
+        {
+           
+            return View("Thanks",book);
+        }
+        public ActionResult Liste()
+        {
+            ViewBag.urunismi = "Mvc Dünyasına Merhaba";
+            ViewBag.adet = 10;
+            return View();
+        }
+
+
+        public ActionResult Detay()
+        {
+            var book = new WebApplication2.Models.Book()
+            {
+                baslik = "MVC Dünyasına Merhaba",
+                aciklama = "MVC adına herşey",
+                fiyat = 100,
+                stokadedi = 100,
+                tur = "Web programlama",
+                yayintarihi = DateTime.Now
+            };
+            ViewData["book"] = book;
+
+            return View(book);
+        }
+    }
+}
---
+++ WebApplication2/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        [DisplayName("Başlık")]
+        public string baslik { get; set; }
+        [DisplayName("Açıklama")]
+        public string aciklama { get; set; }
+        [DisplayName("Kitap türü")]
+        public string tur { get; set; }
+        public DateTime yayintarihi { get; set; }
+        [DisplayName("Fiyat")]
+        public double fiyat { get; set; }
+        [DisplayName("Stok Adeti")]
+        public int stokadedi { get; set; }
+        [DisplayName("Stokta mı?")]
+        public bool discontinued { get; set; }
+    }
+}
